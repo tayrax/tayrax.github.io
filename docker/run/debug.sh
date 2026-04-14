12f6d6251e47102247648e59ea555294862500fe
Jeremías <jrmsdev@gmail.com>
2026-04-14 18:43:46 -0300

    docker run build

--- docker/run/debug.sh
+++ docker/run/debug.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 set -eux
+npm run build
 exec npx vite --host 0.0.0.0 --port 1980 --logLevel info
--- docker/run/devel.sh
+++ docker/run/devel.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 set -eux
+npm run build
 exec npx vite preview --host 0.0.0.0 --port 5173 --logLevel info
