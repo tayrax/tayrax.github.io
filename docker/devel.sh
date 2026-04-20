48fb8ccb2bd2861c3de7d9f3d62c13bcf9150baf
Jeremías <jrmsdev@gmail.com>
2026-04-20 15:41:23 -0300

    claude upgrade

--- docker/devel.sh
+++ docker/devel.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 set -eu
-datadir="${PWD}/docker/data"
+datadir="${HOME}/Docker/claude"
 install -v -d -m 0750 "${datadir}"
-install -v -d -m 0750 "${datadir}/claude"
+install -v -d -m 0750 "${datadir}/config"
 if ! test -s "${datadir}/claude.json"; then
 	touch "${datadir}/claude.json"
 fi
 exec docker run -it --rm -u devel \
 	--hostname tayrax.local \
 	-e "TERM=${TERM}" \
-	-v "${datadir}/claude:/home/devel/.claude" \
+	-v "${datadir}/config:/home/devel/.claude" \
 	-v "${datadir}/claude.json:/home/devel/.claude.json" \
 	-v "${PWD}:/opt/tayrax/site" \
 	--workdir /opt/tayrax/site \
